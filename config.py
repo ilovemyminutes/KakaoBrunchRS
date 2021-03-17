67e24289e30c69855456c796d0bc773aab409210
iloveslowfood <silkstaff@naver.com>
2021-03-17 10:32:32 +0000

    update DataRoots

--- config.py
+++ config.py
@@ -9,8 +9,9 @@ from dataclasses import dataclass
 class DataRoots:
     raw: str = "./raw"
     tfidf: str = (
-        "./raw/preprocessed/metadata_tfidf/metadata_tfidf_vocab7000_aggregation.npz"
+        "preprocessed/metadata_tfidf/metadata_tfidf_vocab7000_aggregation.npz"
     )
-    vocab: str = "./raw/preprocessed/tag_vocab7000.csv"
-    post_id_dec: str = "./raw/preprocessed/post_id_decoding.pickle"
-    post_id_enc: str = "./raw/preprocessed/post_id_encoding.pickle"
+    user_time_read: str = "preprocessed/user_time_read.json"
+    vocab: str = "preprocessed/tag_vocab7000.csv"
+    post_id_dec: str = "preprocessed/post_id_decoding.pickle"
+    post_id_enc: str = "preprocessed/post_id_encoding.pickle"
